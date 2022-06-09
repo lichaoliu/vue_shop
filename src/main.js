106f861502b7d2d732962ddef622c815d0ab29c4
liulichao <liulichao@renrencai.com>
2022-06-09 18:33:15 +0800

    表单布局

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './assets/css/global.css'
 
 const app = createApp(App)
 app.use(router)
