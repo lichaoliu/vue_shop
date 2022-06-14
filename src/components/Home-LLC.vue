0d1fc132c9ee584e1b4f3f14a665abf6abcc64f9
lichaoliu <1147149597@qq.com>
2022-06-14 23:38:49 +0800

    导航栏

--- src/components/Home-LLC.vue
+++ src/components/Home-LLC.vue
@@ -1,18 +1,114 @@
 <template>
-  <div>
-    <el-button type="info"
-               @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div class="header-logo">
+        <img src="../assets/logo2.png"
+             alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info"
+                 @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu active-text-color="#409EFF"
+                 background-color="#333744"
+                 text-color="#fff">
+          <!-- 一级菜单 -->
+          <el-sub-menu :index="item.id + ''"
+                       v-for="item in menuList"
+                       :key="item.id">
+            <template #title>
+              <!-- 图标 -->
+              <el-icon>
+                <shangpin />
+              </el-icon>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="subItem.id + ''"
+                          v-for="subItem in item.children"
+                          :key="subItem.id">
+              <!-- 图标 -->
+              <el-icon>
+                <Menu />
+              </el-icon>
+              <!-- 文本 -->
+              <span>{{subItem.authName}}</span>
+            </el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      menuList: [],
+      iconObj: {
+        125: 'iconfont users',
+        103: 'iconfont tijikongjian',
+        101: 'iconfont shangpin',
+        102: 'iconfont danju',
+        145: 'iconfont baobiao'
+      }
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      console.log(res)
+      this.menuList = res.data
     }
   }
 }
 </script>
 <style lang="less" scoped>
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+
+.el-mai {
+  background-color: #eaedf1;
+}
+
+.home-container {
+  height: 100%;
+}
+
+.header-logo img {
+  height: 50px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
