139cad3cdf24d1569dbac101d0e751460ef90a93
lichaoliu <1147149597@qq.com>
2022-06-15 23:03:57 +0800

    表格区域代码提交

--- src/components/user/Users-LLC.vue
+++ src/components/user/Users-LLC.vue
@@ -6,27 +6,69 @@
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <!-- <el-input placeholder="Please input">
-        <template #prepend>
-
-        </template>
-        <template #append>
-          <el-button :icon="Search" />
-        </template>
-      </el-input> -->
-      <el-input v-model="input1"
-                class="w-50 m-2"
-                size="large"
-                placeholder="Please Input"
-                :suffix-icon="Search" />
-      <el-input v-model="input2"
-                class="w-50 m-2"
-                placeholder="Please Input"
-                :suffix-icon="Search" />
+      <!-- 搜索与添加区域 -->
+      <el-row :gutter="20">
+        <el-col :span="7">
+          <el-input placeholder="请输入内容">
+            <template #append>
+              <el-button icon="Search" />
+            </template>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格区域 -->
+      <el-table :data="userList"
+                stripe
+                border>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="username"
+                         label="姓名" />
+        <el-table-column prop="email"
+                         label="邮箱" />
+        <el-table-column prop="mobile"
+                         label="电话" />
+        <el-table-column prop="role_name"
+                         label="角色" />
+        <el-table-column label="状态">
+          <template v-slot="scope">
+            <el-switch v-model="scope.row.mg_state">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" />
+      </el-table>
     </el-card>
   </div>
 </template>
 <script>
+export default {
+  data () {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created () {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList () {
+      const { data: res } = await this.$http.get('users', { params: this.queryInfo })
+      if (res.meta.status !== 200) return this.$message.error('获取用户列表失败')
+      this.userList = res.data.users
+      this.total = res.data.total
+      console.log(res)
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 </style>
