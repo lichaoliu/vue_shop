be66f636cb3871712931074bd918251c3df427d8
lichaoliu <1147149597@qq.com>
2022-06-20 22:43:35 +0800

    分类参数代码提交

--- src/components/goods/Params-LLC.vue
+++ src/components/goods/Params-LLC.vue
@@ -14,8 +14,30 @@
       <el-row class="cat_opt">
         <el-col>
           <span>选择商品分类:</span>
+          <el-cascader v-model="selectedCateKeys"
+                       :options="cateList"
+                       :props="props"
+                       @change="handleChange" />
         </el-col>
       </el-row>
+      <!-- tab标签区域 -->
+      <el-tabs v-model="activeName"
+               @tab-click="handleTabClick">
+        <el-tab-pane label="动态参数"
+                     name="many">
+          <!-- 添加动态参数 -->
+          <el-button type="primary"
+                     size="small"
+                     :disabled="isBtnDisabled">添加参数</el-button>
+        </el-tab-pane>
+        <el-tab-pane label="静态参数"
+                     name="only">
+          <!-- 添加静态参数 -->
+          <el-button type="primary"
+                     size="small"
+                     :disabled="isBtnDisabled">添加参数</el-button>
+        </el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -23,7 +45,16 @@
 export default {
   data () {
     return {
-      cateList: []
+      cateList: [],
+      props: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      // 级联选择框选中的值
+      selectedCateKeys: [],
+      // 标签区域选择的值
+      activeName: 'many'
     }
   },
   created () {
@@ -37,6 +68,39 @@ export default {
       }
       this.cateList = res.data
       console.log(this.cateList)
+    },
+    // 级联选择框数据变化触发这个函数
+    async handleChange () {
+      if (this.selectedCateKeys.length !== 3) {
+        this.selectedCateKeys = []
+        return
+      }
+      // 根据所选分类id 和 当前所处面板，获取对应参数
+      const { data: res } = await this.$http.get('categories/' + this.cateId + '/attributes', { params: { sel: this.activeName } })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取对应参数失败')
+      }
+      console.log(res)
+    },
+    // 标签区域切换触发的事件
+    handleTabClick () {
+      console.log(this.activeName)
+    }
+  },
+  computed: {
+    // tab区域按钮是否禁用
+    isBtnDisabled () {
+      if (this.selectedCateKeys.length !== 3) {
+        return true
+      }
+      return false
+    },
+    // 当前选中 第三分类id
+    cateId () {
+      if (this.selectedCateKeys.length === 3) {
+        return this.selectedCateKeys[2]
+      }
+      return null
     }
   }
 }
